cc9bd0d9893f78200cf739cb298a825676efe472
Zokerzes <yozhik13@gmail.com>
2023-08-23 23:09:54 +0300

    Добавьте файлы проекта.

---
+++ asp_hw_1/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asp_hw_1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        Random rnd = new Random();
+        public string? letter;
+       
+        char[] letters = Enumerable.Range('a', 'z' - 'a' + 1).Select(c => (char)c).ToArray();
+        public IndexModel()
+        {
+            
+        }
+
+        public void OnGet()
+        {
+            letter = letters[rnd.Next(letters.Length)].ToString();
+        }
+    }
+}
